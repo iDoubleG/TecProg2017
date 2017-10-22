7ac94342147c1ec7294cadeb799d73601cf46e72
pucevinicius <viniciusmorenosilva@hotmail.com>
2017-10-22 00:01:38 -0200

    Atualização do arquivo arena.c

--- parte2/arena.c
+++ parte2/arena.c
@@ -10,46 +10,73 @@ flag -lm (math.h precisa)
 
 #include <stdio.h>
 #include <stdlib.h>
-<<<<<<< HEAD
-#include "maq.h"
+#include <time.h>
+#include <math.h>
 #include "arena.h"
-
 #define GRID_TAM 1000
 #define EXERCITO 500
 #define TIMESTEP 50
-
+#define arenaG arena.grid[i][j].terreno
+#define arenaGR arena.grid[random11][random12].terreno
 
 typedef struct {
-	int grid[GRID_TAM*2][GRID_TAM];
+	Celula grid[GRID_TAM*2][GRID_TAM];
 	int ExercitosAtivos[EXERCITO];
 } Arena;
-=======
-#include <time.h>
-#include <math.h>
 
-int comparador(int rola, int rezendeMeSalva, int base[rezendeMeSalva][rezendeMeSalva]) {
-	if (rola > rezendeMeSalva * rezendeMeSalva) rola = 0;
-	int contador1 = rola / rezendeMeSalva;
-	int contador2 = rola % rezendeMeSalva;
-	if (contador2 == 0) contador1--;
-	else contador2--;
-	if (base[contador1][contador2] > 0) return comparador(base, rola + 1, rezendeMeSalva);
-	else return rola;
-}
 
-int main() {
->>>>>>> 738d14860fa8413418ec78557fc0e164d380e476
+void InsereArena() {
 
-	int rezendeViado = 50;
-	int rezendeGay = 25;
-	int REZENDEDESGRACADOPAUNOCUDOINFERNO = 25;
-	int certo, cu1, xereca2;
-	int cont1, cont2, cont3, cont4, cont5;
+	int times = 25;
+	int timesBases;
+	int random11, random12;
+	int comparador;
 
-	int grid[rezendeViado][rezendeGay];
+	Arena arena;
+
 	srand(time(NULL));
 
-<<<<<<< HEAD
+	// Criacao da arena
+	for (int i = 0; i < (GRID_TAM * 2); i++) {
+		for (int j = 0; j < GRID_TAM; j++) {
+      		if ((i % 2 == 0 && j % 2 != 0) || (j % 2 == 0 && i % 2 != 0)) {
+      			arenaG = NADA;
+      		}
+      		else {
+				int random1 = rand() % 100;
+				if (random1 < 5) {arenaG.terreno = ARMADILHA; arenaT.posi = {i, j};}
+				else if (random1 < 25) {arenaG.terreno = AGUA; arenaT.posi = {i, j};}
+				else if (random1 < 45) {arenaG.terreno = AREIA; arenaT.posi = {i, j};}
+				else if (random1 < 55) {arenaG.terreno = ESTRADA; arenaT.posi = {i, j};}
+				else {arenaG.terreno = RUELA; arenaT.posi = {i, j};}
+      		}
+   		}
+	}
+
+	timesBases = times;
+
+	while (timesBases > 0) {
+		random11 = rand() % (GRID_TAM - 2) + 1;
+		random12 = rand() % (tamanhoY - 2) + 1;
+
+		if (arenaGR.terreno != NADA && arenaGR.terreno != BASE) {
+			arenaGR.terreno = BASE;
+			timesBases--;
+		}
+	}
+}
+
+Maquina *InsereExercito (INSTR diretriz) {
+	Maquina *tropa = cria_maquina(diretriz);
+	tropa->time = i;
+	tropa->posicao = j;
+	return tropa;
+}
+
+void RemoveExercito (Maquina *derrotado) {//poupa trabalho, enviar somente a tropa a ser eliminada
+	destroi_maquina(derrotado);
+}
+
 void Atualiza (Maquina *tropas[256][], int nexercitos) {
 	for (int i=0; i<EXERCITO*nexercitos; i++)
 		for (int j=0; j<nexercitos; j++)
@@ -66,61 +93,4 @@ int main (int ac, char **av){
 	for (int i=0; i<rodadas; i++)
 		Atualiza (tropas);
 	
-=======
-	// Criacao da arena
-	for (int i = 0; i < rezendeViado; i++) {
-		for (int j = 0; j < rezendeGay; j++) {
-      		if ((i % 2 == 0 && j % 2 != 0) || (j % 2 == 0 && i % 2 != 0)) {
-      			grid[i][j] = 0;
-      		}
-      		else {
-				int random1 = rand() % 100;
-				if (random1 < 5) {grid[i][j] = 5; cont5++;}
-				else if (random1 < 25) {grid[i][j] = 4; cont4++;}
-				else if (random1 < 45) {grid[i][j] = 3; cont3++;}
-				else if (random1 < 55) {grid[i][j] = 2; cont2++;}
-				else {grid[i][j] = 1; cont1++;}
-      		}
-      		printf("%d", grid[i][j]);
-   		}
-   		printf("\n");
-	}
-	printf("\n");
-	printf("\n");
-	printf("\n");
-	printf("\n");
-	printf("%d %d %d %d %d", cont1, cont2, cont3, cont4, cont5);
-
-	// Colocar Bases
-	/*int rezendeMeSalva = ceil(sqrt(REZENDEDESGRACADOPAUNOCUDOINFERNO));
-	int base[rezendeMeSalva][rezendeMeSalva];
-
-	for (int i = 0; i < REZENDEDESGRACADOPAUNOCUDOINFERNO; i++) {
-		// Selecão da área
-		int random2 = rand() % (rezendeMeSalva * rezendeMeSalva);
-		int base1 = random2 / rezendeMeSalva;
-		int base2 = random2 % rezendeMeSalva;
-		int retorno = random2 * rezendeMeSalva + base2;
-		if (base2 == 0) base1--;
-		else base2--;
-		if (base[base1][base2] > 0) {
-			certo = comparador(retorno + 1, rezendeMeSalva, base);
-			base1 = certo / rezendeMeSalva;
-			base2 = certo % rezendeMeSalva;
-		}
-		else certo = retorno;
-		base[base1][base2] = 1;
-
-		// Aleatioriedade
-		cu1 = rezendeViado/rezendeMeSalva;
-		xereca2 = rezendeGay/rezendeMeSalva;
-		for (int i = 0; i < cu1; i++) {
-			for (int j = 0; j < xereca2; j++) {
-				int random3 = rand() % (cu1 * xereca2);
-			}
-		}
-	}*/
-
-	return 0;
->>>>>>> 738d14860fa8413418ec78557fc0e164d380e476
 }
