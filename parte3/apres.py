8aa0c14c5d5ed8f80c81562ff44bc384216412cb
pucevinicius <viniciusmorenosilva@hotmail.com>
2017-11-20 04:18:41 -0200

    termino das instrucoes de atacar, recolher, cristais e assassinato. Base quase completamente implementada. Atualizado cmds2 para um teste mais completo e por ter A ENTRADA DE CADA INSTRUÇÃO DEFINIDA, importante para o resto do codigo fazer o pipe

--- parte3/apres.py
+++ parte3/apres.py
@@ -46,11 +46,71 @@ class Robô(pg.sprite.Sprite):
         """
         pg.sprite.Sprite.__init__(s)
         s.image = pg.image.load(img)
+        s.image = pg.transform.scale(s.image, (26, 26))
         s.rect = s.image.get_rect()
 
     def draw(s, i,j):
         """
         Desenha o robô na célula (i,j)
+        """
+        x, y = convert(i,j)
+
+        # fronteiras do sprite
+        l0,h0,l1,h1 = s.rect
+
+        # corrije a posição de acordo com o tamanho do sprite
+        s.rect.topleft = [x-l1/2,y-h1/2]
+        # desenha
+        scr.blit(s.image, s.rect)
+
+
+
+class Base(pg.sprite.Sprite):
+    """
+    Representa um Robô, possui uma imagem
+    """
+    def __init__(s, img):
+        """
+        Construtor, img é o nome do arquivo com a imagem
+        """
+        pg.sprite.Sprite.__init__(s)
+        s.image = pg.image.load(img)
+        s.image = pg.transform.scale(s.image, (26, 26))
+        s.rect = s.image.get_rect()
+
+    def draw(s, i,j):
+        """
+        Desenha o robô na célula (i,j)
+        """
+        x, y = convert(i,j)
+
+        # fronteiras do sprite
+        l0,h0,l1,h1 = s.rect
+
+        # corrije a posição de acordo com o tamanho do sprite
+        s.rect.topleft = [x-l1/2,y-h1/2]
+        # desenha
+        scr.blit(s.image, s.rect)
+
+
+
+class Cristal:
+    """
+    Representa um Cristal, possui uma imagem
+    """
+    def __init__(s):
+        """
+        Construtor, img é o nome do arquivo com a imagem
+        """
+        s.quanti = 0
+        pg.sprite.Sprite.__init__(s)
+        s.image = pg.image.load('footage/cristal.png')
+        s.rect = s.image.get_rect()
+
+
+    def draw(s, i,j):
+        """
+        Desenha o Cristal na célula (i,j)
         """
         x, y = convert(i,j)
 
@@ -75,6 +135,7 @@ class cell:
         s.cx, s.cy = convert(i,j)
         # Cor (pode ser passada para o construtor no futuro)
         s.cor = (200,200,200)
+        s.crist = 0
 
         # Vértices do hexágono
         s.pontos = (
@@ -105,20 +166,80 @@ for i in range(H):
 # lista dos robôs definidos
 robs = []
 
+# lista de bases
+bases = []
+
 # atualiza a janela
 atu()
+
+cont = 0
 
 # Leitura dos comandos pela entrada padrão ou de um arquivo
 for line in fileinput.input():
     r = line.split()
-    #print(len(r))
 
     # linha começando com 'fim', termina o programa
     if r[0] == 'fim':
         break
 
-    if r[0] == 'kill':
+    if r[0] == 'base':
+        r = r[2:]
+        oi, oj = list(map(int, r))
+        bases.append(Robô(r[1]))
 
+        # desenha o robô na célula de destino
+        if 0 <= oi  < W and 0 <= oj < H:
+            bases[cont].draw(oi,oj)
+            cont = cont + 1
+
+        # tempo de espera para atualização, apenas para testes
+        pg.time.wait(1000) 
+        atu()
+        continue
+
+    if r[0] == 'recolhe':
+        r = r[1:]
+        ni, oi, oj = list(map(int, r))
+
+        if arena[oi][oj].quanti - ni <= 0:
+            arena[oi][oj] = cell(oi, oj)
+            arena[oi][oj].draw()
+
+        # tempo de espera para atualização, apenas para testes
+        pg.time.wait(1000) 
+        atu()
+        continue
+
+    if r[0] == 'cristais':
+        r = r[1:]
+        ni, oi, oj = list(map(int, r))
+        arena[oi][oj] = Cristal()
+        arena[oi][oj].quanti = ni
+
+        # desenha o cristal na célula de destino
+        if 0 <= oi  < W and 0 <= oj < H:
+            arena[oi][oj].draw(oi,oj)
+
+        # tempo de espera para atualização, apenas para testes
+        pg.time.wait(1000) 
+        atu()
+        continue
+
+    if r[0] == 'assassinato':
+        r = r[1:]
+        ri, oi, oj = list(map(int, r))
+
+        # remove o robô da lista quando morrer
+        robs.remove(robs[ri])
+
+        # redesenha a célula original (apaga o robô)
+        if 0 <= oi  < W and 0 <= oj < H:
+            arena[oi][oj].draw()
+
+
+        # tempo de espera para atualização, apenas para testes
+        pg.time.wait(1000) 
+        atu()
         continue
 
     # linha começando com 'rob', inclui um robô,
@@ -130,8 +251,8 @@ for line in fileinput.input():
     if r[0] == 'move':
         # linha de movimentação:
         # robô, corrdenadas originais, coordenadas finais
-        s = 
-        ri, oi, oj, di, dj = list(map(int, s))
+        r = r[1:]
+        ri, oi, oj, di, dj = list(map(int, r))
 
 
         # redesenha a célula original (apaga o robô)
